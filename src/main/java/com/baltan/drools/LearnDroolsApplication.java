4ce4ff259dc47c229443617b690df0a14c6f862f
Baltan <617640006@qq.com>
2020-06-27 17:05:14 +0800

    增加Redis相关配置

--- src/main/java/com/baltan/drools/LearnDroolsApplication.java
+++ src/main/java/com/baltan/drools/LearnDroolsApplication.java
@@ -1,7 +1,16 @@
 package com.baltan.drools;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class LearnDroolsApplication {
@@ -10,4 +19,33 @@ public class LearnDroolsApplication {
         SpringApplication.run(LearnDroolsApplication.class, args);
     }
 
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        /**
+         * 使用Jackson2JsonRedisSerializer来执行序列化和反序列化
+         */
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+                new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        /**
+         * 指定要序列化的属性，ANY是都有，包括private和public
+         */
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        /**
+         * @deprecated objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+         * 指定序列化输入的类型，类必须是非final修饰的
+         */
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
 }
